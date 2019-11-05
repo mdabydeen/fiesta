1965146100df7eb763fc0e67a737aa33581907b0
Michael Dabydeen <mdabydeen@gmail.com>
2019-11-05 15:18:37 -0500

    API Spec: Added more API specs.
    Starting to flesh out what the APIs would be
    
    Author: Michael Dabydeen <mdabydeen@gmail.com>

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,11 +21,30 @@ Route::post('register', 'Auth\AuthController@register');
 Route::post('forgot-password', 'Auth\AuthController@forgotPassword');
 Route::post('reset-password', 'Auth\AuthController@resetPassword');
 
-Route::group(['middleware' => 'auth:api'],
-function() {
+// Route::group(['middleware' => 'auth:api'],
+// function() {
+
+    /** Auth */
     Route::get('logout', 'Auth\AuthController@logout');
+
+    /**
+     * Myself
+     */
+    Route::get('me', 'Post\PostControllor@createPost');
+    Route::get('me/timeline', 'Post\PostControllor@createPost');
+
+    /** User */
     Route::get('user', 'User\UserController@user');
-});
+    Route::get('user/:id', 'User\UserController@getUserID');
+    Route::post('user/:id/follow', 'User\UserController@follow');
+    Route::get('user/:id/timeline', 'User\UserController@getUserTimeline');
+
+    /**
+     * Post
+     */
+    Route::post('post', 'Post\PostController@createPost');
+
+// });
 
 // Route::middleware('auth:api')->get('user', function (Request $request) {
 //     return "Hello World!";
