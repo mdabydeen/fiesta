ebacfc66ea76d06e4a37daacd1c5a2443cabfcba
Michael Dabydeen <mdabydeen@gmail.com>
2019-11-05 22:56:38 -0500

    PostController: Added the API for posts.
    Posts now have it's own API
    
    Author: Michael Dabydeen <mdabydeen@gmail.com>

---
+++ backend/app/Http/Controllers/Post/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+
+class PostController extends Controller
+{
+    //
+    public function createPost(Request $request)
+    {
+
+
+        return response()->json();
+    }
+}
